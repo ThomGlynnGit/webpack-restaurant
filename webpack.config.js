76f959e4fc520e12e39e0b9453d6fce921084907
Thomas Taylor <98777656+ThomGlynnGit@users.noreply.github.com>
2026-02-26 11:42:36 +0000

    link html file to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,16 @@
 import path from "node:path";
 
 export default {
-  mode: "development",
-  entry: "./src/page.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
-  },
+    mode: "development",
+    entry: "./src/page.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(import.meta.dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
 };
