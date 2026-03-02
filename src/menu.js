b86c32283784f9cec3932152693efa563e172068
Thomas Taylor <98777656+ThomGlynnGit@users.noreply.github.com>
2026-03-02 15:05:48 +0000

    display mains items on menu

--- src/menu.js
+++ src/menu.js
@@ -13,6 +13,7 @@ export default function menuLoad(){
     menuCont.className = "menu container"
     contentCont.appendChild(menuCont)
 
+    //starters elements
     const startersCont = document.createElement("div")
     const startersHead = document.createElement("h3")
     const fishStarter = document.createElement("p")
@@ -35,6 +36,30 @@ export default function menuLoad(){
     startersCont.appendChild(vegStarter)
     startersCont.appendChild(oliveStarter)
     menuCont.appendChild(startersCont)
+
+    //mains elements
+    const mainsCont = document.createElement("div")
+    const mainsHead = document.createElement("h3")
+    const fishMain = document.createElement("p")
+    const vegMain = document.createElement("p")
+    const pizzaMain = document.createElement("p")
+
+    mainsCont.className = "menu mains"
+    mainsHead.className = "menu-section mains"
+    fishMain.className = "mains item"
+    vegMain.className = "mains item"
+    pizzaMain.className = "mains item"
+
+    mainsHead.textContent = "Mains"
+    fishMain.textContent = "orange orzo with oregano octopus"
+    vegMain.textContent = "fiery fig fusilli"
+    pizzaMain.textContent = "cheesy tomato bread (pizza)"
+
+    mainsCont.appendChild(mainsHead)
+    mainsCont.appendChild(fishMain)
+    mainsCont.appendChild(vegMain)
+    mainsCont.appendChild(pizzaMain)
+    menuCont.appendChild(mainsCont)
 
 
 }
